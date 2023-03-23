edd2a0b422de2f093ecdca6ba00e1e834efca984
Svetoslav Zhelev <105590138+SNZhelev21@users.noreply.github.com>
2023-03-23 20:09:34 +0200

    Add Loading menu function and int main()

--- 2223-9th-grade-sprint-math-games/2048Game.cpp
+++ 2223-9th-grade-sprint-math-games/2048Game.cpp
@@ -1,51 +1,54 @@
 #include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <stdlib.h>
+#include <Windows.h> 
+
 using namespace std;
-int borders[4][4];
-int moveRow[] = { 0, 1, 0, -1 };
-int moveCol[] = { 1, 0, -1, 0 };
-//0 - right, 1 - down, 2 - left, 3 - up
 
-void newGameStart()
+int index = 2;
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_ENTER 13
+#define KEY_BACKSPACE 8
+
+void loadingMenu()
 {
-	for (int i = 0; i < 4; ++i)
+	system("COLOR 0e");
+	system("cls");
+
+	cout << setw(200) << " __   __  ___   _______  _______  ___   _______  __    _        ___   __   __  _______  _______  _______  _______  ___   _______  ___      _______ " << endl;
+	cout << setw(200) << "|  |_|  ||   | |       ||       ||   | |       ||  |  | |      |   | |  |_|  ||       ||       ||       ||       ||   | |  _    ||   |    |       |" << endl;
+	cout << setw(200) << "|       ||   | |  _____||  _____||   | |   _   ||   |_| |      |   | |       ||    _  ||   _   ||  _____||  _____||   | | |_|   ||   |    |    ___|" << endl;
+	cout << setw(200) << "|       ||   | | |_____ | |_____ |   | |  | |  ||       |      |   | |       ||   |_| ||  | |  || |_____ | |_____ |   | |       ||   |    |   |___ " << endl;
+	cout << setw(200) << "|       ||   | |_____  ||_____  ||   | |  |_|  ||  _    |      |   | |       ||    ___||  |_|  ||_____  ||_____  ||   | |  _   | |   |___ |    ___|" << endl;
+	cout << setw(200) << "| ||_|| ||   |  _____| | _____| ||   | |       || | |   |      |   | | ||_|| ||   |    |       | _____| | _____| ||   | | |_|   ||       ||   |___ " << endl;
+	cout << setw(200) << "|_|   |_||___| |_______||_______||___| |_______||_|  |__|      |___| |_|   |_||___|    |_______||_______||_______||___| |_______||_______||_______|" << endl;
+
+	SetConsoleCP(437);
+	SetConsoleOutputCP(437);
+	int bar1 = 177, bar2 = 219;
+
+	cout << "\n\n\n\t\t\t\tLoading...";
+	cout << "\n\n\n\t\t\t\t";
+
+	for (int i = 0; i < 25; i++)
+		cout << (char)bar1;
+
+	cout << "\r";
+	cout << "\t\t\t\t";
+	for (int i = 0; i < 25; i++)
 	{
-		for (int j = 0; j < 4; ++j)
-		{
-			borders[i][j] = 0;
-		}
+		cout << (char)bar2;
+		Sleep(150);
 	}
-}
-bool DoMove(int liner, int column, int nliner, int ncolumn)
-{
-	if (nliner < 0 || ncolumn < 0 || nliner > 4 || ncolumn >= 4 || (borders[liner][column] != borders[nliner][ncolumn] and borders[nliner][ncolumn] != 0))
-	{
-		return false;
-	}
-	else
-	{
-		return true;
+
+	cout << "\n\t\t\t\t" << (char)1 << "!";
+	Sleep(250);
+	system("COLOR E");
 }
 
-	int main()
-	{
-		char buttonCommand[128];
-		buttonCommand['d'] = 0;
-		buttonCommand['s'] = 1;
-		buttonCommand['a'] = 2;
-		buttonCommand['w'] = 3;
-		newGameStart();
-		while (true)
-		{
-			char commands;
-			cin >> commands;
-			if (commands == 'n')
-			{
-				newGameStart();
-			}
-			else if (commands == 'q')
-			{
-				break;
-			}
-			cout << "When the code is ready here will be the output";
-		}
-	}
+int main()
+{
+	//the rest of the code will be added soon.
